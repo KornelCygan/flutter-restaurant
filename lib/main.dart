9d1582d0a3130bc2024166c84e88c788713502f8
Kornel Cygan <31730479+KornelCygan@users.noreply.github.com>
2020-02-04 07:55:49 +0100

    added topbar with scaffold style

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/shared/custom.style.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,9 +7,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-      child: Text('Hello World') ,
-      ),
+      theme: cAppThemeData,
+      title: cAppTitle,
+      debugShowCheckedModeBanner: false,
+      home: FoodHome(),
     );
+  }
+}
+
+class FoodHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return Center(
+    //   child: Text('Hello World') ,
+    //   );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text(cAppTitle),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.shopping_cart, color: Colors.white, size: 30.0),
+              tooltip: 'Shop',
+              onPressed: () {
+                Navigator.pushNamed(context, '/shopcart');
+            }),
+            IconButton(
+              icon: Icon(Icons.restaurant_menu, color: Colors.white, size: 30.0),
+              tooltip: 'Menu',
+              onPressed: () {
+                Navigator.pushNamed(context, '/orders');
+            }),
+            IconButton(
+              icon: Icon(Icons.settings, color: Colors.white, size: 30.0),
+              tooltip: 'Setup',
+              onPressed: () {
+                Navigator.pushNamed(context, '/setmenu');
+            })
+          ],
+        ) ,
+      );
   }
 }
---
+++ lib/shared/custom.style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const cAppTitle = 'Restaurant App';
+
+var cAppThemeData = ThemeData(
+        primaryColor: Colors.blueAccent,
+        primarySwatch: Colors.blue,
+        buttonColor: Colors.blue,
+        buttonTheme: const ButtonThemeData(textTheme: ButtonTextTheme.primary),
+      );
