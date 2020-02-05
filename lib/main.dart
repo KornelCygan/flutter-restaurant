8bf6dd9832c3f0553bbb52bf244961f8edd335a7
Kornel Cygan <31730479+KornelCygan@users.noreply.github.com>
2020-02-05 22:07:22 +0100

    added body and bottomBar with sample buttons

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,6 @@ class MyApp extends StatelessWidget {
 class FoodHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // return Center(
-    //   child: Text('Hello World') ,
-    //   );
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightBlueAccent,
@@ -45,7 +42,47 @@ class FoodHome extends StatelessWidget {
                 Navigator.pushNamed(context, '/setmenu');
             })
           ],
-        ) ,
+        ),
+        body: Center(
+          child: Text('Hello World') ,
+        ),
+        drawer: Drawer(child: Text('Test nav text')),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.green,
+          child: Icon(Icons.shopping_cart, color: Colors.blueGrey, size: 30.0),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 4.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.receipt, color: Colors.grey, size: 30.0),
+                onPressed: () {
+                  //todo
+                  showDialog(
+                    context: context,
+                    builder: (_) => new AlertDialog(
+                      title: Text('test1'),
+                      content: Text('test2'),
+                  ));
+              }),
+              IconButton(
+                icon: Icon(Icons.sms, color: Colors.grey, size: 30.0),
+                onPressed: () {
+                  //todo
+                  showDialog(
+                    context: context,
+                    builder: (_) => new AlertDialog(
+                      title: Text('test1'),
+                      content: Text('test2'),
+                  ));
+              })
+            ],
+            )
+        ),
       );
   }
 }
